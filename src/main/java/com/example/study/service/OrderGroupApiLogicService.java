47969e4e1004d51696f30f2d74ff5c5df1b0270c
mxxnkyung@naver.com <mxxnkyung@naver.com>
2021-05-30 00:11:01 +0900

    21.05.29 부트 OrderGroup API CRUD

---
+++ src/main/java/com/example/study/service/OrderGroupApiLogicService.java
@@ -0,0 +1,98 @@
+package com.example.study.service;
+
+import com.example.study.interpace.CrudInterface;
+import com.example.study.model.entity.OrderGroup;
+import com.example.study.model.network.Header;
+import com.example.study.model.network.request.OrderGroupApiRequest;
+import com.example.study.model.network.response.ItemApiResponse;
+import com.example.study.model.network.response.OrderGroupApiResponse;
+import com.example.study.repository.OrderGroupRepository;
+import com.example.study.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class OrderGroupApiLogicService implements CrudInterface<OrderGroupApiRequest, OrderGroupApiResponse> {
+
+    private final OrderGroupRepository orderGroupRepository;
+
+    private final UserRepository userRepository;
+
+    @Override
+    public Header<OrderGroupApiResponse> create(Header<OrderGroupApiRequest> request) {
+
+        OrderGroupApiRequest body = request.getData();
+        OrderGroup orderGroup = OrderGroup.builder()
+                .status(body.getStatus())
+                .orderType(body.getOrderType())
+                .revAddress(body.getRevAddress())
+                .revName(body.getRevName())
+                .paymentType(body.getPaymentType())
+                .totalPrice(body.getTotalPrice())
+                .totalQuantity(body.getTotalQuantity())
+                .orderAt(LocalDateTime.now())
+                .user(userRepository.getOne(body.getUserId()))
+                .build();
+
+        OrderGroup newOrderGroup = orderGroupRepository.save(orderGroup);
+
+        return response(newOrderGroup);
+    }
+
+    @Override
+    public Header<OrderGroupApiResponse> read(Long id) {
+
+        return orderGroupRepository.findById(id)
+                .map(this::response)
+                .orElseGet(() -> Header.ERROR("데이터 없음"));
+    }
+
+    @Override
+    public Header<OrderGroupApiResponse> update(Header<OrderGroupApiRequest> request) {
+
+        OrderGroupApiRequest body = request.getData();
+        return orderGroupRepository.findById(body.getId())
+                .map(orderGroup -> {
+                        orderGroup
+                                .setRevAddress(body.getRevAddress())
+                                ;
+                        return orderGroup;
+                })
+                //.map(newOrderGroup -> orderGroupRepository.save(newOrderGroup)) 대체 가능
+                .map(orderGroupRepository::save)
+                .map(this::response)
+                .orElseGet(() -> Header.ERROR("없데이트!"));
+    }
+
+    @Override
+    public Header delete(Long id) {
+        return orderGroupRepository.findById(id)
+                .map(orderGroup -> {
+                    orderGroupRepository.delete(orderGroup);
+                    return Header.OK();
+                })
+                .orElseGet(() -> Header.ERROR("삭제 할 데이터가 없음"));
+    }
+
+
+    private Header<OrderGroupApiResponse> response(OrderGroup orderGroup) {
+        OrderGroupApiResponse orderGroupApiResponse = OrderGroupApiResponse.builder()
+                .id(orderGroup.getId())
+                .status(orderGroup.getStatus())
+                .orderType(orderGroup.getOrderType())
+                .revAddress(orderGroup.getRevAddress())
+                .revName(orderGroup.getRevName())
+                .paymentType(orderGroup.getPaymentType())
+                .totalPrice(orderGroup.getTotalPrice())
+                .totalQuantity(orderGroup.getTotalQuantity())
+                .orderAt(orderGroup.getOrderAt())
+                .userId(orderGroup.getUser().getId())
+                .build();
+        return Header.OK(orderGroupApiResponse);
+    }
+
+}
